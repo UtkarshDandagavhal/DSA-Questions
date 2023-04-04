c455e8721f40ce0d40776da1ab9c32c6658ca8ab
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-04-04 19:36:05 +0530

    Update move_neg_ele.cpp

--- move_neg_ele.cpp
+++ move_neg_ele.cpp
@@ -1,48 +1,37 @@
 #include <iostream>
 using namespace std;
-void shifting(int arr[],int left,int right){
-    while(left<=right){
-        if(arr[left] < 0 && arr[right] < 0){
-            left = left + 1;
-        }
-        else if(arr[left] > 0 && arr[right] < 0){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
 
-            left = left + 1;
-            right = right-1;
-        }
-        else if(arr[left] > 0 && arr[right] > 0){
-            right = right - 1;
-        }
+void MoveAllNegtoleft(int arr[], int n){
+  int left = 0;
+  int right = n-1;
 
-        else{
-            left = left + 1;
-            right = right - 1;
-        }
+  while(left < right){
+    if(arr[left] < 0){
+      left++;
     }
-}
-
-void display(int arr[],int right){
-    for(int i = 0;i<=right;++i){
-       cout << arr[i] << " ";
+    else if(arr[right] > 0){
+      right--;
     }
-    cout<<endl;
+    else{
+      swap(arr[left],arr[right]);
+    }
+  }
 }
 
 int main() {
-    int n;
-    cout<<"Enter the n values: "<<endl;
-    cin>>n;
-    int arr[n];
+  int arr[] = {1,2,-3,-4,5,6,-7};
 
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    // calling function
+  int n =  sizeof(arr)/sizeof(int);
 
-    shifting(arr,0,n-1);
-    display(arr,n-1);
-    return 0;
+
+  MoveAllNegtoleft(arr,n);
+
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+
+  cout<<endl;
+
+
+  return 0;
 }
