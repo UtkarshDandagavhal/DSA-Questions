2b6dfcf651da6f95b0882f1ac74add487134e0c0
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-04-06 15:22:13 +0530

    Update nearly_sorted_arr.cpp

--- nearly_sorted_arr.cpp
+++ nearly_sorted_arr.cpp
@@ -32,7 +32,7 @@ int binarySearch(vector<int>arr,int target){
 int main() {
   vector<int> arr {10,3,40,20,50,70,80};
 
-  int target = 402;
+  int target = 40;
 
   int ans = binarySearch(arr,target);
   cout<<"index of "<<target<<" is "<<ans<<endl;
