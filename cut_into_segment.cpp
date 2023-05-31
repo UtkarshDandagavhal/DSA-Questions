9ee9f5e77521520de55eef201b634a70e4967512
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-05-31 20:32:16 +0530

    Create cut_into_segment.cpp

---
+++ cut_into_segment.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<limits.h>
+using namespace std;
+
+int solve(int n,int x,int y,int z){
+  // base case
+  if(n == 0){
+    return 0;
+  }
+  if(n < 0){
+    return INT_MIN;
+  }
+
+  // one case solve
+  int ans1 = solve(n-x,x,y,z) + 1;
+  int ans2 = solve(n-y,x,y,z) + 1;
+  int ans3 = solve(n-z,x,y,z) + 1;
+
+  int ans = max(ans1,max(ans2,ans3));
+  return ans;
+
+}
+
+int main() {
+  int n = 7;
+  int x = 5;
+  int y = 2;
+  int z = 2;
+
+  int ans = solve(n,x,y,z);
+  
+
+  if(ans < 0){
+    ans = 0;
+  }
+  cout<<"Answer is: "<<ans;
+  return 0;
+}
