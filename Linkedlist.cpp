bfafb3018d7903a85318e6bfe81f9f8c78ce804e
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-09-05 18:54:13 +0530

    Update Linkedlist.cpp

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -17,6 +17,16 @@ class Node{
     }
 };
 
+int findlength(Node* &head){
+  int len = 0;
+  Node* temp = head;
+  while(temp!=NULL){
+    temp = temp->next;
+    len++;
+  }
+  return len;
+}
+
 // insert at head
 void insertAthead(Node* &head,Node* &tail,int data){
 
@@ -58,6 +68,41 @@ void insertAttail(Node* head,Node* &tail,int data){
 
 }
 
+void insertAtpostion(int data,int postion,Node* head,Node* tail){
+  if(head == NULL){
+    Node* newnode = new Node(data);
+    head = newnode;
+    tail = newnode;
+  }
+
+  if(postion == 0){
+    insertAthead(head,tail,data);
+    return;
+  }
+
+  int len = findlength(head);
+  if(postion >= len){
+    insertAttail(head,tail,data);
+    return;
+  }
+
+  // find the prev and curr node
+  int i = 1;
+  Node* prev = head;
+  while(i < postion){
+    prev=prev->next;
+    i++;
+  }
+
+  Node* curr = prev->next;
+  // step-2
+  Node* newnode = new Node(data);
+  // step-3
+  newnode->next = curr;
+  // step-4
+  prev->next = newnode;
+}
+
 void print(Node* head){
     Node* temp = head;
     while(temp != NULL){
@@ -71,13 +116,16 @@ int main() {
   Node* head = NULL;
   Node* tail = NULL;
 
-//   insertAthead(head,tail,20);
-//   insertAthead(head,tail,30);
-//   insertAthead(head,tail,40);
-//   insertAthead(head,tail,50);
-//   insertAthead(head,tail,60);
+  insertAthead(head,tail,20);
+  insertAthead(head,tail,30);
+  insertAthead(head,tail,40);
+  insertAthead(head,tail,50);
+  insertAthead(head,tail,60);
   insertAttail(head,tail,70);
 
+  print(head);
+  cout<<endl;
+  insertAtpostion(101,2,head,tail);
   print(head);
   return 0;
 }
